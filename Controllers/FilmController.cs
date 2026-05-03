54fe80e4b82168187b4c8a17813924789865dc75
yohanabarus887 <yohanabarus887@gmail.com>
2026-05-03 18:32:39 +0700

    first commmit

---
+++ Controllers/FilmController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TP_MODUL10_103022400097;
+
+namespace TP_MODUL10_103022400097.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FilmController : ControllerBase
+    {
+        private static List<Film> _filmList = new List<Film>
+        {
+            new Film { Judul = "Inception", Sutradara = "Christopher Nolan", Tahun = "2010", Genre = "Sci-Fi", Rating = "9.0" },
+            new Film { Judul = "Interstellar", Sutradara = "Christopher Nolan", Tahun = "2014", Genre = "Sci-Fi", Rating = "8.7" },
+            new Film { Judul = "Parasite", Sutradara = "Bong Joon-ho", Tahun = "2019", Genre = "Thriller", Rating = "8.6" }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Film>> GetAll()
+        {
+            return Ok(_filmList);
+        }
+
+        
+        [HttpGet("{index}")]
+        public ActionResult<Film> GetByIndex(int index)
+        {
+            if (index < 0 || index >= _filmList.Count) return NotFound();
+
+            return Ok(_filmList[index]);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Film baru)
+        {
+            _filmList.Add(baru);
+            return Ok(_filmList); 
+        }
+
+        [HttpDelete("{index}")]
+        public ActionResult Delete(int index)
+        {
+            if (index < 0 || index >= _filmList.Count) return NotFound();
+
+            _filmList.RemoveAt(index);
+            return Ok(new { message = "Film berhasil dihapus" });
+        }
+    }
+}
---
+++ Film.cs
@@ -0,0 +1,11 @@
+﻿namespace TP_MODUL10_103022400097
+{
+    public class Film
+    {
+        public string Judul { get; set; }
+        public string Sutradara { get; set; }
+        public string Tahun { get; set; }
+        public string Genre { get; set; }
+        public string Rating { get; set; }
+    }
+}
